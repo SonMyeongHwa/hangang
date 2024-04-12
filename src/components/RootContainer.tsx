47d0718d746d36837ff4f9662f1fa27e8f3561bb
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-04-12 15:53:45 +0900

    Feat: layout.tsx>queryClient 생성

---
+++ src/components/RootContainer.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+
+const RootContainer = ({ children }: any) => {
+  const [rendered, setRendered] = useState(false);
+  const queryClient = new QueryClient();
+
+  useEffect(() => {
+    setRendered(true);
+  }, []);
+  
+  return (
+    rendered && (
+      <QueryClientProvider client={queryClient}>
+          {children}
+      </QueryClientProvider>
+    )
+  );
+};
+
+export default RootContainer;
