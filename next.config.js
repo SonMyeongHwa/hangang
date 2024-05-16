1633d042742ccbfe9c05260226f49a6e92dc563f
MyeonghwaSon <myeonghwa0116@gmail.com>
2024-05-16 17:17:55 +0900

    next.config 수정

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
     return [
       {
         source: "/api/seoul-open-api/:path*",
-        destination: "http://openapi.seoul.go.kr:8088/:path*", // 프록시할 대상 서버 주소
+        destination: "https://openapi.seoul.go.kr:8088/:path*", // 프록시할 대상 서버 주소
       },
     ];
   },
